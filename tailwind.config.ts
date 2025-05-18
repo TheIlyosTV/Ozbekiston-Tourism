5cbdb70be660089f25e84ed5a847073cd39f1352
TheIlyosTV <hakkermaktabi19@gmail.com>
2025-05-18 05:01:48 +0500

    ts -> js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "tailwindcss"
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
   content: [
     "app/**/*.{ts,tsx}",
@@ -62,5 +62,3 @@ const config: Config = {
   },
   plugins: [require("tailwindcss-animate")],
 }
-
-export default config
